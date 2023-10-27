cc32979c3b0d1306caceb5f49c2196bf5d0113f8
Timothy Patel <timdp24@gmail.com>
2023-10-26 21:34:45 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,59 @@
+import type { Metadata } from 'next'
+import { Toaster } from 'sonner';
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { ConvexClientProvider } from '@/components/providers/convex-provider';
+import { EdgeStoreProvider } from '@/lib/edgestore';
+import { ModalProvider } from '@/components/providers/modal-provider';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Manage Memos App',
+  description: 'App to manage memos.',
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg",
+      }
+    ]
+  }
+};
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <EdgeStoreProvider>
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="momotion-theme"
+            >
+              <Toaster position='bottom-center' />
+              <ModalProvider />
+            {children}
+          </ThemeProvider>
+          </EdgeStoreProvider>
+        </ConvexClientProvider>
+        
+      </body>
+    </html>
+  )
+};
---
+++ convex/auth.config.js
@@ -0,0 +1,12 @@
+export default {
+    providers: [
+      {
+        domain: "https://splendid-stag-68.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+
+
+
